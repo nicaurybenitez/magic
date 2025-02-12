556da567af0383f9d267c0a892c31d8fde17e85a
Nicaury Benitez <nbenitez@gmail.com>
2025-02-12 23:33:51 +0100

    Fix  networking modules

--- terraform/modules/cloud_run/main.tf
+++ terraform/modules/cloud_run/main.tf
@@ -1,3 +1,4 @@
+# modules/cloud_run/main.tf
 resource "google_cloud_run_service" "service" {
   name     = "magic-service-${var.environment}"
   location = var.region
@@ -19,8 +20,20 @@ resource "google_cloud_run_service" "service" {
     }
   }
 
+  lifecycle {
+    create_before_destroy = true
+  }
+
   traffic {
     percent         = 100
     latest_revision = true
   }
+}
+
+# IAM policy for Cloud Run service
+resource "google_cloud_run_service_iam_member" "public_access" {
+  service  = google_cloud_run_service.service.name
+  location = google_cloud_run_service.service.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
